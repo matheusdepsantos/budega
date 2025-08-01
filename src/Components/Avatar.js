d353689f365ab14b8ef4e73a9facb5499b9f2455
Matheus de Paula <matheus.psantoz@gmail.com>
2025-08-01 13:13:03 -0300

    feat: conclusão da estilização do header

---
+++ src/Components/Avatar.js
@@ -0,0 +1,17 @@
+const styles = {
+  avatar: {
+    width: "44px",
+    height: "44px",
+    borderRadius: "50%",
+    backgroundColor: "#ffffff",
+    margin: "0px 21px 0 88px",
+  },
+};
+
+function Avatar() {
+  return <div style={styles.avatar}></div>
+}             
+
+
+
+export default Avatar;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,4 @@
+import Avatar from "./Avatar";
 import Menu from "./Menu";
 
 const styles = {
@@ -5,13 +6,65 @@ const styles = {
     width: "100%",
     height: "53px",
     backgroundColor: "#158A8A",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  menuInput: {
+    display: "flex",
+    alignItems: "center",
+  },
+
+  image: {
+    width: "103px",
+    height: "33px",
+    marginLeft: "26px",
+  },
+
+  input: {
+    width: "305px",
+    height: "30px",
+    borderRadius: "3px",
+    border: "none",
+    marginLeft: "45px",
+  },
+
+  favoiriteAvatar: {
+    display: "flex",
+    alignItems: "center",
+    color: "#ffffff"
+  },
+
+  favoritos: {
+    margin: "16px 76px 16px 0px",
+    fontSize: "14px",
+  },
+
+  iconBag: {
+  },
+
+  sacola: {
+    fontSize: "14px",
   },
 };
 
 function Header() {
   return (
     <div style={styles.header}>
-      <Menu />
+      <div style={styles.menuInput}>
+        <Menu />
+        <img src="../../logo.png" alt="" style={styles.image} />
+        <input type="text" name="" id="" style={styles.input} />
+      </div>
+      <div style={styles.favoiriteAvatar}>
+        <span style={styles.favoritos}>Favoritos</span>
+        <span style={styles.iconBag} class="material-symbols-outlined">
+          local_mall
+        </span>
+        <span style={styles.sacola}>Sacola</span>
+        <Avatar />
+      </div>
     </div>
   );
 }
